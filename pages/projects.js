c41f803ba486aa0ebe81c8c24b5e92b43bd18474
claynaut <jpesc002@ucr.edu>
2021-07-27 19:18:06 -0700

    Add links

--- pages/projects.js
+++ pages/projects.js
@@ -19,6 +19,7 @@ export default function Projects() {
           <div className={styles.subtitle}>In the Works</div>
           <div className={styles.section}>
             <ProjectBlurb
+              link="https://jspescas.io"
               title="Portfolio"
               date="July 2021 - Present"
               caption="Revamping my first portfolio, with new styles and more features."
@@ -28,6 +29,7 @@ export default function Projects() {
               <div>Framer Motion</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="https://cutiehack2021.vercel.app/"
               title="CutieHack 2021 Website"
               date="June 2021 - Present"
               caption="Developing the frontend and the backend of the website for CutieHack, a beginner-oriented hackathon at UCR."
@@ -39,6 +41,7 @@ export default function Projects() {
               <div>TypeScript</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="/projects"
               title="A Web Planner App"
               date="July 2021 - Present"
               caption="Building a web application on a larger scale than I've done before. Geared towards anyone who organizes their day on the go."
@@ -51,6 +54,7 @@ export default function Projects() {
               <div>TypeScript</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="/projects"
               title="DiamondHacks 2021 Website"
               date="July 2021 - Present"
               caption="Building the frontend and the backend of the website for DiamondHacks, a new hackathon at UCR."
@@ -65,6 +69,7 @@ export default function Projects() {
           <div className={styles.subtitle}>Upcoming</div>
           <div className={styles.section}>
             <ProjectBlurb
+              link="/projects"
               title="CitrusHack 2022 Website"
               date="2021-2022"
               caption="Designing and developing the website for CitrusHack, a large-scale hackathon at UCR."
@@ -76,6 +81,7 @@ export default function Projects() {
               <div>TypeScript</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="/projects"
               title="BioHack 2022 Website"
               date="2021-2022"
               caption="Building the website for BioHack, a healthcare hackathon at UCR."
@@ -90,6 +96,7 @@ export default function Projects() {
           <div className={styles.subtitle}>Completed</div>
           <div className={styles.section}>
             <ProjectBlurb
+              link="https://dota-2-analyzer-frontend.vercel.app/"
               title="Dota 2 Analyzer"
               date="Jan 2021 - May 2021"
               caption="Built the frontend of a web application using an in-memory database, used to analyze statistics for Dota 2. Developed as a final project with a team of four, for a Software Engineering course."
@@ -98,15 +105,17 @@ export default function Projects() {
               <div>CSS Modules</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="https://jspescasio-fe.vercel.app/"
               title="First Portfolio"
               date="Jan 2021 - July 2021"
-              caption="Built my first website using Gatsby."
+              caption="Built my first website using Gatsby. Wanted to develop a website from scratch, trying out different frameworks."
             >
               <div>Gatsby.JS</div>
               <div>CSS Modules</div>
               <div>Framer Motion</div>
             </ProjectBlurb>
             <ProjectBlurb
+              link="https://biohack2021-jsdev.vercel.app/"
               title="BioHack 2021 Website"
               date="Jan 2021 - May 2021"
               caption="Helped develop the frontend for the BioHack website with the web dev committee."
