c9693f62f2773708f19c73c1e2547c66f1ab7b60
claynaut <jpesc002@ucr.edu>
2021-07-27 23:18:06 -0700

    Finish blog

--- pages/blog/making-my-first-portfolio.js
+++ pages/blog/making-my-first-portfolio.js
@@ -15,7 +15,7 @@ export default function Blog() {
       <main className={styles.main}>
         <div>
           <div className={styles.title}>Making my First Portfolio</div>
-          <p className={styles.time}>July 27 2021 • 2 min read</p>
+          <p className={styles.time}>July 27 2021 • 1 min read</p>
           <div className={styles.section}>
             <div className={styles.subtitle}>Getting Into Web Development</div>
             <p>
@@ -25,13 +25,19 @@ export default function Blog() {
               At this point, my only experience with web dev was learning HTML/CSS online to try and make a web app back in high school, which never pushed through. I was working with one HTML file and had no clue about any frameworks or other tools.
             </p>
             <p>
-              P
+              So, I wanted to try something new, and I loved it.
             </p>
           </div>
           <div className={styles.section}>
             <div className={styles.subtitle}>Why Make a Website?</div>
             <p>
-              Eat
+              What better way to learn something new than just diving right in? So, I decided to try building my own website using Gatsby.
+            </p>
+            <p>
+              My experience from BioHack made me want to learn more about web development. And making my own website from scratch definitely led me to learning more about other frameworks and toolchains, along with challenging my design skills. 
+            </p>
+            <p>
+              With that, I made my own personal space on the internet. Hope you like my website!
             </p>
           </div>
         </div>
--- pages/blog/remaking-my-portfolio.js
+++ pages/blog/remaking-my-portfolio.js
@@ -19,13 +19,32 @@ export default function Blog() {
           <div className={styles.section}>
             <div className={styles.subtitle}>Why Did I Revamp It?</div>
             <p>
-              Eat
+              I wanted to try something new (again). And as I learned more in web development since the first time I made my initial portfolio, I wanted to make some more improvements and add more features.
             </p>
           </div>
           <div className={styles.section}>
             <div className={styles.subtitle}>What&apos;s New?</div>
+            <div className={styles.subsubtitle}>Added Themes</div>
             <p>
-              Eat
+              I always wanted to try making color themes for a website, like light mode and dark mode. So, I finally figured it out and added it to this new portfolio.
+            </p>
+            <div className={styles.subsubtitle}>Started a Blog</div>
+            <p>
+              Thought that writing a blog would be a new and fun experience for me. I&apos;m personally not the best at telling stories, so I hope that as I continue writing this blog that I&apos;ll get better at it.
+            </p>
+            <p>
+              This blog also lets me have something to do on a regular basis (hopefully), other than coding. So, I hope you enjoy reading about my experiences!
+            </p>
+            <div className={styles.subsubtitle}>A Cleaner UI/UX</div>
+            <p>
+              Personally enjoyed the creative aspect of my previous portfolio with overlaying divs. Yet, I wanted to try making something cleaner while keeping some parts from my previous portfolio, like the side navbar and blurbs.
+            </p>
+            <p>
+              And I&apos;m definitely proud of how I redid my Projects and About sections.
+            </p>
+            <div className={styles.subsubtitle}>More About Me</div>
+            <p>
+              Added more to my About section, so you can learn more about me as a person. Previously, my old portfolio only showcased a few details about what I&apos;ve done and what I plan to do. Now, I&apos;d like to share my experiences with a timeline and this blog.
             </p>
           </div>
         </div>
