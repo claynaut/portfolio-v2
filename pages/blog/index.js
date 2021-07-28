3b45516ee56a3cb3360cb02835bd8a0be1e5ff77
claynaut <jpesc002@ucr.edu>
2021-07-28 15:44:22 -0700

    Add icons

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -10,6 +10,8 @@ export default function Blog() {
       <Head>
         <title>J.S. Pescasio | Blog</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/maskable-512x512.png" purpose="any maskable" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/favicon-512x512.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
@@ -22,6 +24,18 @@ export default function Blog() {
         <div>
           <div className={styles.title}>Blog</div>
           <p>Occasionally writing about what I&apos;ve done and what I find interesting.</p>
+          <BlogBlurb
+            link="/blog"
+            title="Trying Out for Web Dev Lead"
+            date="August 2021"
+            caption="Getting outside of my comfort zone with my desire to learn more."
+          />
+          <BlogBlurb
+            link="/blog"
+            title="Joining BioHack"
+            date="July 2021"
+            caption="Going over my experience with joining the BioHack web dev committee."
+          />
           <BlogBlurb
             link="/blog/remaking-my-portfolio"
             title="Revamping My Portfolio"
