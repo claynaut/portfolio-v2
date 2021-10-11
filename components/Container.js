64092437fec7b8cb7d7ec517d131bcd07ac4480d
claynaut <jpesc002@ucr.edu>
2021-10-10 20:04:49 -0700

    Fix margin

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
   return (
-    <div className='flex flex-col justify-between lg:mx-22 my-6 mt-22 lg:mt-0 p-6 rounded-lg bg-primary dark:bg-primary-dark shadow-light dark:shadow-dark'>
+    <div className='flex flex-col justify-between lg:mx-22 my-6 mt-22 lg:mt-6 p-6 rounded-lg bg-primary dark:bg-primary-dark shadow-light dark:shadow-dark'>
       {children}
     </div>
   )
