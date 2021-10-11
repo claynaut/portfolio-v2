4eca94b41e1d11fd90c89848902321a7d91705f5
claynaut <jpesc002@ucr.edu>
2021-10-10 23:43:23 -0700

    Align icons

--- components/TimeBlurb.js
+++ components/TimeBlurb.js
@@ -1,7 +1,7 @@
 export default function TimeBlurb(props) {
   return (
     <div className='flex ml-7'>
-      <div className='mr-4 text-xl text-text-secondary dark:text-text-secondary-dark'>
+      <div className='mt-1 mr-4 text-xl text-text-secondary dark:text-text-secondary-dark'>
         {props.icon}
       </div>
       <div className='flex flex-col mb-5'>
