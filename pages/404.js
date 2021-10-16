3873b62985fe6b43d9b57706e66d721cada6477d
claynaut <hello@jspescas.io>
2021-10-16 09:51:00 -0700

    Add error pages

---
+++ pages/404.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Container from '../../components/Container'
+
+export default function Error404() {
+  return (
+    <Container>
+      <Head>
+        <title>J.S. Pescasio | 404</title>
+      </Head>
+      <section className='flex flex-col-reverse md:flex-row w-full'>
+        <div className='w-full'>
+          <h1 className='mb-4 font-bold text-4xl text-accent dark:text-accent-dark'>
+            404
+          </h1>
+          <p className='my-4'>
+            This page does&apos;nt exist.
+          </p>
+        </div>
+      </section>
+    </Container>
+  )
+}
