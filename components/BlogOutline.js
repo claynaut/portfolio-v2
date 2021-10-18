702a41c83d8259a207ef2e424e5c1b8c69ea6b16
claynaut <jpesc002@ucr.edu>
2021-10-18 00:54:13 -0700

    Adjust margin/padding

--- components/BlogOutline.js
+++ components/BlogOutline.js
@@ -9,11 +9,11 @@ export default function BlogOutline({ bookmarks }) {
   return (
     <>
       {/* desktop blog outline */}
-      <div className='z-[100] fixed top-1/2 right-2 hidden 2xl:flex flex-col justify-center p-2.5 rounded-lg rounded-bl-2xl bg-primary shadow-shadow font-medium transform -translate-y-1/2'>
-        <h2 className='flex gap-2 items-center mt-0 mb-2 text-xl text-accent'>
+      <div className='z-[100] fixed top-1/2 right-2 hidden 2xl:flex flex-col justify-center max-w-[15rem] p-2.5 rounded-lg rounded-bl-2xl bg-primary shadow-shadow font-medium transform -translate-y-1/2'>
+        <h2 className='flex gap-2 items-center mt-0 mb-2 pt-2 pl-2 text-xl text-accent'>
           <BiListUl className='text-2xl' /> Outline
         </h2>
-        <div className='flex gap-1.5 flex-col'>
+        <div className='flex gap-1.5 flex-col px-2 pb-2'>
           { bookmarks.map((bookmark) =>
             <NavLink 
               activeClass='text-accent'
@@ -23,8 +23,8 @@ export default function BlogOutline({ bookmarks }) {
               offset={-90}
               duration={500}
               className={
-                'text-base hover:text-accent cursor-pointer transition-all duration-150 '
-                + (bookmark.value.includes('subheading') && 'ml-6')
+                'hover:text-accent cursor-pointer transition-all duration-150 '
+                + (bookmark.value.includes('subheading') ? 'ml-6 text-sm' : 'text-base')
               }
             >
               {bookmark.title}
@@ -35,14 +35,14 @@ export default function BlogOutline({ bookmarks }) {
       {/* mobile blog outline */}
       <div 
         className={
-          'z-[100] fixed bottom-2 right-2 flex 2xl:hidden flex-col justify-end items-start p-2.5 rounded-lg rounded-tl-2xl bg-primary shadow-shadow font-medium overflow-hidden '
+          'z-[100] fixed bottom-2 right-2 flex 2xl:hidden flex-col justify-end items-start max-w-[15rem] p-2.5 rounded-lg rounded-tl-2xl bg-primary shadow-shadow font-medium overflow-hidden '
           + (!open && 'w-[3.75rem] h-[3.75rem]')
         }
       >
-        <h2 className='flex gap-2 items-center mt-0 mb-2 text-xl text-accent'>
+        <h2 className='flex gap-2 items-center mt-0 mb-2 pt-2 pl-2 text-xl text-accent'>
           <BiListUl className='text-2xl' /> Outline
         </h2>
-        <div className='flex gap-1.5 flex-col'>
+        <div className='flex gap-1.5 flex-col px-2 pb-2'>
           { bookmarks.map((bookmark) =>
             <NavLink 
               activeClass='text-accent'
@@ -53,7 +53,7 @@ export default function BlogOutline({ bookmarks }) {
               duration={500}
               className={
                 'text-base hover:text-accent cursor-pointer transition-all duration-150 '
-                + (bookmark.value.includes('subheading') && 'ml-6')
+                + (bookmark.value.includes('subheading') ? 'ml-6 text-sm' : 'text-base')
               }
             >
               {bookmark.title}
@@ -61,7 +61,7 @@ export default function BlogOutline({ bookmarks }) {
           )}
         </div>
         <div
-          className='flex self-end max-w-min mt-2 p-2 rounded-lg rounded-tl-2xl hover:bg-nav-button text-2xl cursor-pointer'
+          className='flex self-end max-w-min p-2 rounded-lg rounded-tl-2xl hover:bg-nav-button text-2xl cursor-pointer'
           onClick={() => setOpen(!open)}
         >
           { open
