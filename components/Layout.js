03c7bd1b881aeb580d792411f05c00c36324465b
claynaut <hello@jspescas.io>
2021-10-19 18:53:35 -0700

    Remove attribute

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,6 @@ export default function Layout({ children }) {
   return (
     <>
       <motion.main
-        layoutId={router.pathname}
         initial={{ opacity: 0, x: -200, y: 0 }}
         animate={{ opacity: 1, x: 0, y: 0 }}
         exit={{ opacity: 0, x: 0, y: -100 }}
