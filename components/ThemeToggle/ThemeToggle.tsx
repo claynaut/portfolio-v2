853de0d396d83e6eff93da0e794cec8b8743b05f
claynaut <hello@jspescas.io>
2022-06-12 20:08:54 -0700

    Implement theme toggle

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+import { BiSun, BiMoon } from 'react-icons/bi'
+
 export const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
+
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) return null
+
   return (
-    <>
-    </>
+    <button
+      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      className='fixed top-4 right-4 p-3 rounded-full shadow-md'
+    >
+      <div className='text-2xl'>
+        {theme === 'light' ? <BiSun /> : <BiMoon />}
+      </div>
+    </button>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import { Navbar } from 'components/Navbar'
+import { ThemeToggle } from 'components/ThemeToggle'
 
 import 'tailwindcss/tailwind.css'
 import 'tailwind.source.css'
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={false}>
       <Navbar />
+      <ThemeToggle />
       <Component {...pageProps} />
     </ThemeProvider>
   )
